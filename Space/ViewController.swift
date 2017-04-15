019799247e0ea8b214a5ab49f1bd69910757d6a2
Mustafa Yusuf <mustafaaliasger.2014@vit.ac.in>
2017-04-15 21:07:01 +0530

    Initial Commit

---
+++ Space/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Space
+//
+//  Created by Mustafa Yusuf on 15/04/17.
+//  Copyright © 2017 Mustafa Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
